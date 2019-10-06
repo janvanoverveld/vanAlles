9839f1b994ab42440edd673f2269e6d4bfabf5e3
Jan van Overveld <janvanoverveld@hotmail.com>
2019-10-06 12:36:28 +0200

    update

--- AliceAndBob2/alice.ts
+++ AliceAndBob2/alice.ts
@@ -1,6 +1,8 @@
 import {ADD,RES,BYE} from './Message';
 import {receiveMessageServer,sendMessage} from './httpCommunication';
 
+const hostAlice='localhost';
+const portAlice=30001;
 const hostBob='localhost';
 const portBob=30002;
 const value: ()=>number = ()=>Math.floor(Math.random() * 5);
@@ -10,13 +12,13 @@ async function startProtocol() {
       const add:ADD = new ADD(value(),value());
       sendMessage(hostBob, portBob, add );
       const res = <RES> await receiveMessageServer.waitForMessage();
-      console.log(`send an ADD to Bob with values ${add.value1} and ${add.value2}, received ${res.sum}`);
+      console.log(`Send an ADD to Bob with values ${add.value1} and ${add.value2}, received ${res.sum}`);
    }
    await sendMessage( hostBob, portBob, new BYE() );
    console.log('the protocol stops for Alice');
    receiveMessageServer.terminate();
 }
 
-receiveMessageServer.start(30001);
+receiveMessageServer.start(portAlice);
 
 startProtocol();
--- AliceAndBob2/bob.ts
+++ AliceAndBob2/bob.ts
@@ -3,6 +3,8 @@ import {receiveMessageServer,sendMessage} from './httpCommunication';
 
 const hostAlice='localhost';
 const portAlice=30001;
+const hostBob='localhost';
+const portBob=30002;
 
 async function startProtocol() {
    let msg = <Message> await receiveMessageServer.waitForMessage();
@@ -10,14 +12,14 @@ async function startProtocol() {
       const add:ADD = <ADD>msg;
       const res = new RES(add.value1+add.value2);
       sendMessage(hostAlice, portAlice, res );
-      console.log(`an ADD received with ${add.value1} and ${add.value2}, send a RES with ${res.sum} back`);
+      console.log(`An ADD received with ${add.value1} and ${add.value2}, send a RES with ${res.sum} back`);
       msg = <Message> await receiveMessageServer.waitForMessage();
    }
    console.log('the protocol stops for Bob');
    receiveMessageServer.terminate();
 }
 
-receiveMessageServer.start(30002);
+receiveMessageServer.start(portBob);
 
 startProtocol();
 
