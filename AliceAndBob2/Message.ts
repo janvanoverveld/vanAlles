25170e5974d9a8b0e13dd442b6695f69a72b4c25
Jan van Overveld <janvanoverveld@hotmail.com>
2019-10-06 13:52:03 +0200

    update

--- AliceAndBob2/Message.ts
+++ AliceAndBob2/Message.ts
@@ -1,21 +1,23 @@
-export abstract class Message {
+abstract class Message {
     constructor(public name:string){};
 }
 
-export class ADD extends Message{
+class ADD extends Message{
     constructor(public value1:number, public value2:number){
         super(ADD.name);
     }
 }
 
-export class RES extends Message {
+class RES extends Message {
     constructor(public sum:number){
         super(RES.name);
     }
 }
 
-export class BYE extends Message {
+class BYE extends Message {
     constructor(){
         super(BYE.name);
     }
 }
+
+export {Message, ADD, RES, BYE}
